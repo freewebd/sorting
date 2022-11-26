92ab82556f2d4a8e4fd75837942671f868b7abb6
flysawed <flysawed@gmail.com>
2022-11-26 23:34:10 +0200

    Sorting one column

--- index.php
+++ index.php
@@ -4,36 +4,13 @@ require('data.php');
 require('app.php');
 
 $columnHeaders = array_keys($array[1]);
-
-function createHref($getParam, $columnHeader)
-{
-    if (empty($getParam) ) {
-        $getParam = ['key1' => $columnHeader, 'sort1' => 'abc'];
-        return http_build_query($getParam, '', '&amp;');
-    }
-    if ($getParam['key1'] === $columnHeader && !array_key_exists('key2', $getParam)) {
-        $getParam['sort1'] = 'cba';
-        return http_build_query($getParam, '', '&amp;');
-    }
-    if ($getParam['key1'] !== $columnHeader && !array_key_exists('key2', $getParam)) {
-        $getParam = ['key2' => $columnHeader, 'sort2' => 'abc'];
-        return http_build_query($getParam, '', '&amp;');
-    }
-    if ($getParam['key2'] === $columnHeader ) {
-        $getParam['sort2'] =  'cba';
-        return http_build_query($getParam, '', '&amp;');
-    }
-
-
-}
-
 ?>
 
 <table border="2">
     <tr>
         <?php foreach ($columnHeaders as $columnHeader) : ?>
             <th>
-                <a href="?<?= createHref($_GET, $columnHeader) ?>">
+                <a href="?key=<?= $columnHeader ?>&sort=<?= sorting_direction($columnHeader, $_GET['key'], $_GET['sort']) ?>">
                     <?= $columnHeader ?>
                 </a>
             </th>
